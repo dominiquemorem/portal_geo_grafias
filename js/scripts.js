9cdfeccae1e2ded57fb5d116512cdb8208069a05
Dominique Morem <domi.bcastro@gmail.com>
2023-06-12 16:57:29 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    $("#tours_virtuais ul").bxSlider({
+        auto: true,
+        mode: 'fade',
+        speed: 700,
+        responsive: true,
+    });
+
+    $("#noticias ul").bxSlider({
+        auto: true,
+        mode: 'fade',
+        speed: 700,
+        responsive: true,
+    });
+
+    $("#sites_uteis ul").bxSlider({
+        auto: true,
+        mode: 'fade',
+        speed: 700,
+        responsive: true,
+    });
+
+    $('#galeria_world_maps').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true,
+            titleSrc: function (item) {
+                return item.el.attr('title') + ' &middot; <a class="image-source-link" href="' + item.el.attr('data-source') + '" target="_blank">image source</a>';
+            }
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function (element) {
+                return element.find('img');
+            }
+        }
+    });
+
+});
